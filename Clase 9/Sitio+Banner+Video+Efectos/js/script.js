de81cbcc254e73b0e3b718af16fea4b287587264
Giovanny Suarez <suarezkrac@gmail.com>
2015-05-22 12:00:47 -0500

    Ejercicio

--- Clase 9/Sitio+Banner+Video+Efectos/js/script.js
+++ Clase 9/Sitio+Banner+Video+Efectos/js/script.js
@@ -9,6 +9,35 @@ function paginaCargada () {
 	
 	$('#name').on("blur", desenfoqueName);
 }
+function scrollPagina (argument) {
+	var posScroll = $(window).scrollTop();
+
+	if (posScroll > 50) {
+		$('#logo').css({
+			"width": "50px",
+			"margin-right": "30px"
+		});
+		$('h1').hide();
+		$('.botones').css({
+			"margin-top": "0"
+		});
+		$('header').addClass("changeHeader");
+	}
+	if (posScroll < 50) {
+		$('#logo').css({
+			"width": "130px",
+			"margin-right": "0px"
+		});
+		$('h1').show();
+		
+		$('.botones').css({
+			"margin-top": "10px"
+		});
+		$('header').removeClass("changeHeader");
+	}
+
+}
+
 function verVideo () {
 	$('#colombia_video').addClass("ver_video");
 }
