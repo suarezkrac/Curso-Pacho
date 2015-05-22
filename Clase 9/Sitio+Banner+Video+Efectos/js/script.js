84342eb27590935597dff39f9a5ee4c950d7a3db
Giovanny Suarez <suarezkrac@gmail.com>
2015-05-22 11:15:56 -0500

    Actualizacion

---
+++ Clase 9/Sitio+Banner+Video+Efectos/js/script.js
@@ -0,0 +1,24 @@
+$(document).on("ready", paginaCargada);
+function paginaCargada () {
+
+	$(window).scroll(scrollPagina);
+	
+	$('#equipos article').addClass("animated zoomInUp");
+
+	$('#equipos li').on("click", verVideo);
+	
+	$('#name').on("blur", desenfoqueName);
+}
+function verVideo () {
+	$('#colombia_video').addClass("ver_video");
+}
+
+function desenfoqueName () {
+	var nombre = $('#name').val();
+	if (nombre == "") {
+		alert("El nombre es obligatorio!!");
+		$('#name').addClass("bordesRojos");
+	}else{
+		$('#name').removeClass("bordesRojos");
+	}
+}
